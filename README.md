dec824abf032d519ca72bb344f91ae1530e3cc35
jinwoo725 <gmlrhs6399@naver.com>
2025-10-31 12:49:28 +0900

    0.3v Update

--- README.md
+++ README.md
@@ -1,17 +1,52 @@
+Base64_Python<br>
+-
+
 A beginner's base64 encoder/decoder in Python
 
-class Base64<br>
--_encoding(type:string) > type:string<br>
--_decoding(type:string) > type:string
-
 install<br>
--pip install base64_python
+-
+Linux&MacOS
+-
+**Bash/Zsh**
+```bash
+pip3 install base64_python
+```
+Windows
+-
+**CMD/PowerShell**
+```cmd
+pip install base64_python
+```
+
+function<br>
+-
+- **encode(str|bytes) -> str**<br>
+- **decode(str|bytes) -> str**<br>
+- **test() -> str**
 
 Exam<br>
--import base64_python<br>
--base64 = base64_python.Base64()<br>
--print(base64._encoding("test"))<br>
--print(base64._decoding("dGVzdA=="))
+-
+**Example**<br>
+```python
+import base64_python
+base64 = base64_python.Base64()
+print(base64.encode("Exam"))
+print(base64.decode("RXhhbQ=="))
+print(base64.test())
+```
+**Output**<br>
+```output
+RXhhbQ==
+Exam
++--------------------------------+
+| Input Text : Exam              |
+| Encoding   : RXhhbQ==          |
+| Decoding   : Exam              |
+| Time       : 0.00004 seconds   |
++--------------------------------+
+```
 
+License<br>
+-
 
-
+This project is licensed under the terms of the MIT license.
--- base64_python/module.py
+++ base64_python/module.py
@@ -11,40 +11,59 @@ class Base64:
         self._base64.append("+")
         self._base64.append("/")
     
-    def _encoding(self, data:str) -> str:
+    def encode(self, data:str|bytes) -> str:
+        """
+        This function is an encoder function
+        Encode a string|bytes to base64.
+        """
         try:
-            ord_data = data.encode("utf-8")
+            ord_data = data if isinstance(data, bytes) else data.encode("utf-8")
             bin_data = [bin(i) for i in ord_data]
-            bin_data = [i.replace("0b","") for i in bin_data]
-            bin_data = ["0" * abs(len(i)-8) + i if len(i) != 8 else i for i in bin_data]
-            bin_data = [bin_data[i:i+3] for i in range(0, len(bin_data), 3)]
-            int_data = [self._split(i) for i in bin_data]
-            str_data = "=" * abs(len(int_data[-1]) - 4)
-            int_data = [j for i in int_data for j in i]
-            return "".join([self._base64[i] for i in int_data]) + str_data
-        except:
-            return ""
+            bin_data = ["0" * (8 - len(i[2:])) + i[2:] if len(i[2:]) != 8 else i[2:] for i in bin_data]
+            str_data = "=" * abs(3 - len(bin_data) % 3) if len(bin_data) % 3 != 0 else ""
+            bin_data = "".join(bin_data)
+            bin_data = [bin_data[i:i+6] for i in range(0, len(bin_data), 6)]
+            bin_data = [i + "0" * (6 - len(i)) if len(i) != 6 else i for i in bin_data]
+            int_data = [int(i, 2) for i in bin_data]
+            return "".join([self._base64[i] for i in int_data]) + str_data if data else ""
+        except Exception as e:
+            raise e
 
-    def _decoding(self, data:str) -> str:
-        int_data = [self._base64.index(i) for i in data if i != "="]
-        bin_data = [bin(i) for i in int_data]
-        bin_data = [i.replace("0b","") for i in bin_data]
-        bin_data = ["0" * abs(len(i)-6) + i if len(i) != 6 else i for i in bin_data]
-        bin_data = "".join(bin_data)
-        bin_data = [bin_data[i:i+8] for i in range(0, len(bin_data), 8)]
-        int_data = [int(i, 2) for i in bin_data]
-        ord_data = bytearray(int_data)
-        return ord_data.decode("utf-8")
+    def decode(self, data:str|bytes) -> str:
+        """
+        This is a decoder function
+        Decodes a base64 encoded string|bytes.
+        """
+        try:
+            ord_data = data.decode("utf-8") if isinstance(data, bytes) else data
+            int_data = [self._base64.index(i) for i in ord_data if i != "="]
+            bin_data = [bin(i) for i in int_data]
+            bin_data = ["0" * (6 - len(i[2:])) + i[2:] if len(i[2:]) != 6 else i[2:] for i in bin_data]
+            bin_data = "".join(bin_data)
+            bin_data = [bin_data[i:i+8] for i in range(0, len(bin_data), 8)]
+            int_data = [int(i, 2) for i in bin_data]
+            ord_data = bytearray(int_data)
+            return ord_data.decode("utf-8")
+        except Exception as e:
+            raise e
     
-    def _split(self, data:str) -> str:
-        bin_data = "".join(data)
-        bin_data = [bin_data[i:i+6] for i in range(0, len(bin_data), 6)]
-        bin_data = ["0b" + i + "0" * abs(len(i)-6) if len(i) != 6 else "0b" + i for i in bin_data]
-        int_data = [int(i,2) for i in bin_data]
-        return int_data
-    
+def test() -> str:
+    """
+    This is a test function
+    """
+    from time import time
+    start = time()
+    base64 = Base64()
+    str_data = base64.encode(b"Exam") # or str_data = base64.encode("Exam")
+    base64_data = base64.decode(b"RXhhbQ==") # or base64_data = base64.decode("RXhhbQ==")
+    end = time()
+    return f"""+--------------------------------+
+| Input Text : Exam              |
+| Encoding   : {str_data}          |
+| Decoding   : {base64_data}              |
+| Time       : {end - start:.5f} seconds   |
++--------------------------------+"""
 
 if __name__ == "__main__":
-    base64 = Base64()
-    print(base64._encoding("test"))
-    print(base64._decoding("dGVzdA=="))
+    test = test()
+    print(test)
